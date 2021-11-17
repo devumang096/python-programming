754f049d282d25a52672d251f85643f8faf64ea5
Umang Devanshu <81811004+devumang096@users.noreply.github.com>
2021-11-17 22:25:18 +0530

    basic program

---
+++ BASICS/Basic.py
@@ -0,0 +1,12 @@
+#1st part
+val =input("What is you name?")
+var= input("your registeration no.")
+print(val)
+print(var)
+x=125
+#2nd part
+print(type(x))
+#3rd part
+print(bin(x))
+print(oct(x))
+print(hex(x))
