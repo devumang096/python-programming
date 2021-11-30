0744ebac801991c1316dd109b4d803321bc8c758
Umang Devanshu <81811004+devumang096@users.noreply.github.com>
2021-11-30 11:00:42 +0530

    types of operators

---
+++ Variables & DataTypes/02_operators.py
@@ -0,0 +1,31 @@
+a=3
+b=4
+
+#Arithmetic operators
+print("the value of 3+4 is",a+b)
+print("the value of 3-4 is",a-b)
+print("the value of 3*4 is",a*b)
+print("the value of 3/4 is",a/b)
+
+#Assignment operators
+a=34
+a += 12
+a -= 12
+a *= 12
+a /= 12
+print(a)
+
+# Comparison operators
+b = (4>7)
+b = (4<7)
+b = (14==7)
+b = (14!=7)
+print(b)
+
+# Logical operators
+bool1 = True
+bool2 = False
+print("the value of bool1 & bool2 is",(bool1 and bool2))
+print("the value of bool1 & bool2 is",(bool1 or bool2))
+print("the value of bool1 & bool2 is",(not bool2))
+
