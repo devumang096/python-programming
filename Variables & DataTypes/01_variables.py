52ed9b8d8ca15e0c25db057028f2c4c907020ba8
devumang096 <devumang096@gmail.com>
2021-11-30 10:37:27 +0530

    modifying expressions

--- Variables & DataTypes/01_variables.py
+++ Variables & DataTypes/01_variables.py
@@ -4,6 +4,16 @@ a = 'umang'
 a = '''umang'''
 b = 345
 c = 43.54
+d=False
+# d=None (NoneType)
+# Printing the variables
 print(a)
 print(b)
 print(c)
+print(d)
+
+#Printing the type of variables
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
