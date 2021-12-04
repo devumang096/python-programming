68727fa7e2631c7acc74b40e2b994af0fe2d7a65
devumang096 <devumang096@gmail.com>
2021-12-04 16:27:39 +0530

    modified slicing

--- Strings/02_string_slicing.py
+++ Strings/02_string_slicing.py
@@ -1,5 +1,13 @@
-greeting = "Good Morning, "
-name = "Umang"
+# greeting = "Good Morning, "
+#name = "Umang"
 # print(type(name))
-c = greeting + name
-print(c)
+
+#Concatinating the strings
+# c = greeting + name
+# print(c)
+name = "Umang"
+print(name[1])
+
+# name[3]="d"-->Does not work
+
+print(name[0:2])
