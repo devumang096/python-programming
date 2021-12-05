b95c367c3685545f3cae689c6c72263aa2dec66e
devumang096 <devumang096@gmail.com>
2021-12-05 22:20:06 +0530

    Negative index

--- Strings/02_string_slicing.py
+++ Strings/02_string_slicing.py
@@ -11,3 +11,10 @@ print(name[1])
 # name[3]="d"-->Does not work
 
 print(name[0:2])
+
+# print(name[1:4])
+# print(name[:4]) #is same as name[0:4]
+# print(name[1:]) #is same as name[1:5]
+
+c = name[-4:-1] #is same as name[1:4]
+print(c)
